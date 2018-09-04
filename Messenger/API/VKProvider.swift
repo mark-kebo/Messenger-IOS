a83db9b0549c5c3056a284a784374da9f5b7ba97
Mark Kebo <arolij.corp@gmail.com>
2018-09-04 16:47:05 +0300

    small impl

--- Messenger/API/VKProvider.swift
+++ Messenger/API/VKProvider.swift
@@ -9,7 +9,7 @@
 import Foundation
 import VK_ios_sdk
 
-class VKProvider: FriendsListProviderProtocol {
+class VKProvider: FriendsListProviderProtocol, MessagesProviderProtocol {
     
     public func getFriendsList(treatmentFriends: @escaping ([Person]) -> Void) {
         var bioFriends = [Person]()
@@ -17,8 +17,10 @@ class VKProvider: FriendsListProviderProtocol {
             let json = response?.json as! Dictionary<String, Any>
             let items = json["items"] as! Array<Dictionary<String, Any>>
             items.forEach {
-                bioFriends.append(Person(name: $0["first_name"] as! String, surname: $0["last_name"] as! String,
-                                         avaImgUrl: $0["photo_50"] as! String, isOnline: $0["online"] as! Bool))
+                bioFriends.append(Person(name: $0["first_name"] as! String,
+                                         surname: $0["last_name"] as! String,
+                                         avaImgUrl: $0["photo_50"] as! String,
+                                         isOnline: $0["online"] as! Bool))
             }
             treatmentFriends(bioFriends)
         }, errorBlock: { (error) in
@@ -27,6 +29,23 @@ class VKProvider: FriendsListProviderProtocol {
     }
     
     public func getMessagesList(treatmentMessages: @escaping ([PreliminaryMessage]) -> Void) {
-        
+        // get list prev messages
+        var conversations = [PreliminaryMessage]()
+        VKRequest(method: "messages.getConversations", parameters: ["fields":"photo_50"]).execute(resultBlock: { (response) in
+            let json = response?.json as! Dictionary<String, Any>
+            let items = json["items"] as! Array<Dictionary<String, Any>>
+            items.forEach {
+                print($0["last_message"])
+//                conversations.append(PreliminaryMessage(person: Person(name: $0["user_id"] as! String,
+//                                                                       surname: "",
+//                                                                       avaImgUrl: "",
+//                                                                       isOnline: false),
+//                                                        lastMessage: $0["text"] as! String,
+//                                                        lastDateMessage: $0["date"] as! Date))
+            }
+            treatmentMessages(conversations)
+        }, errorBlock: { (error) in
+            print("Error: \(String(describing: error))")
+        })
     }
 }
